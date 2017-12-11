ee6b741c0615d02a6fc4bfcd5061c3eb3b161b5c
bjavier9 <31942457+bjavier9@users.noreply.github.com>
2017-12-10 21:18:22 -0500

    Create recursoshumanos.py

---
+++ recursoshumanos.py
@@ -0,0 +1,37 @@
+if __name__ == '__main__':
+   print("linea de comando que controla los recursos humanos")
+   print("elija su opcion")
+while True:
+    print("\nMENU de registro \n 1- agregar colaborador \n 2- visualizar \n 3-eliminar \n 4-salir")
+    op = input()
+    if op == "1":
+       archivo = open("recursoshumanos.csv" , "w")
+       nombre = input("escribe tu nombre: ")
+       cedula = input("escribe tu cedula: ")
+       departamento = input("departamento: ")
+       labor = input("labor: ")
+       salario = input("escribe tu salario: ")
+
+       archivo.write(nombre)
+       archivo.write(", ")
+       archivo.write(cedula)
+       archivo.write(", ")
+       archivo.write(departamento)
+       archivo.write(", ")
+       archivo.write(labor)
+       archivo.write(", ")
+       archivo.write(salario)
+       archivo.write("\n ")
+       archivo.close()
+    elif op == "2":
+       print("mostrar registro")
+       archivo = open("recursoshumanos.csv","r")
+       print (archivo.read())
+       archivo.close()
+    elif op == "3":
+        print("eliminar archivo")
+        archivo = open("recursoshumanos.csv")
+        archivo.truncate()
+        print("registro eliminados")
+    elif op=="4":
+        break
