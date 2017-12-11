047f547e9417aff6c90c02e56570704c5c885666
bjavier9 <31942457+bjavier9@users.noreply.github.com>
2017-12-10 21:52:48 -0500

    Update volumen1.py

--- volumen1.py
+++ volumen1.py
@@ -1,22 +1,62 @@
-class Volumen(object):
+pi = 3.14159265359
+class Esfera(object):
+    def __init__(x, r):
+        x.radio = r
+        x.volumen = 0
 
-    def vol_figura():
-        print("escriba la figura")
-        figura = input("")
-        if figura == "cilindro":
-            radio = int(input("introduce el valor del radio"))
-            altura = int(input("intruduce el valor de la latura "))
-            print(((3.14)**2)*(radio)*(altura))
-        elif figura == "cono":
-            radio = int(input("introduce el valor del radio"))
-            altura = int(input("intruduce el valor de la latura "))
-            print((1/3)*(3.14)*(altura)*(radio)**2)
-        elif figura == "esfera":
-            radio = int(input("introduce el valor del radio"))
-            print(4/3*3.14*radio**3)
-        else:
-            print("tas mal papa")
+    def obtvol(x):
+        r = x.radio
+        x.volumen = (4 / 3) * pi * (r ** 3)
+        return x.volumen
 
+
+class Cilindro(object):
+    def __init__(x, r, h):
+        x.radio = r
+        x.altura = h
+        x.volumen = 0
+
+
+    def obtvol(x):
+        r = x.radio
+        h = x.altura
+        x.volumen = pi * (r ** 2) * h
+        return x.volumen
+
+
+class Cono(object):
+    def __init__(x, r, h):
+        x.radio = r
+        x.altura = h
+        x.volumen = 0
+
+    def obtvol(x):
+        r = x.radio
+        h = x.altura
+        x.volumen = (1 / 3) * pi * (r ** 2) * h
+        return x.volumen
 if __name__ == '__main__':
-    luisa = Volumen
-    luisa.vol_figura()
+ while True :
+        print("Ingrese la figura geometrica que desee encontrar 1\nCilindro, \n 2-Cono \n 3- Esfera \n 4-salir ")
+        op = input()
+
+        if op == "1":
+            print("     Esfera")
+            r = int(input("Ingrese el radio de la esfera: "))
+            s = Esfera(r)
+            print("El volumen de la esfera es: ", s.obtvol())
+        elif op == "2":
+            print("     Cono")
+            r = int(input("Ingrese el radio de el cono: "))
+            h = int(input("Ingrese la altura de el cono: "))
+            d = Cono(r, h)
+            print("El volumen de el cono es: ", d.obtvol())
+        elif op == "3":
+            print("     Cilindro")
+            r = int(input("Ingrese el radio de el cilindro: "))
+            h = int(input("Ingrese la altura de el cilindro: "))
+            c = Cilindro(r, h)
+            print("El volumen de el cilindro es: ", c.obtvol())
+        elif op == "4":
+            break
+
